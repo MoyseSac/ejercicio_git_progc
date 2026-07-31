670d1eb9282fd99cfa739363b6781460b9e5c9c2
MoyseSac <moisessac@Moisess-MacBook-Pro.local>
2026-07-30 22:42:39 -0600

    refactor: estructura

--- comisiones.py
+++ comisiones.py
@@ -18,37 +18,44 @@ TASA_COMISION_BAJA = 0.05
 UMBRAL_BONO = 50000
 MONTO_BONO = 500
 
-def calcular_comisiones():
-    total_pagado = 0
+
+def calcular_comision(monto_ventas):
+    # si vendio mas de 30000, comision del 8%; si no, del 5%
+    if monto_ventas > UMBRAL_COMISION_ALTA:
+        comision = monto_ventas * TASA_COMISION_ALTA
+    else:
+        comision = monto_ventas * TASA_COMISION_BAJA
+    return round(comision, 2)
+
+
+def calcular_bono(monto_ventas):
+    # el bono es de 300
+    if monto_ventas > UMBRAL_BONO:
+        return MONTO_BONO
+    return 0
+
+
+def calcular_total_vendedor(monto_ventas):
+    return round(calcular_comision(monto_ventas) + calcular_bono(monto_ventas), 2)
+
+
+def calcular_comisiones(vendedores):
+    # recorre la lista
+    return [(nombre, calcular_total_vendedor(ventas)) for nombre, ventas in vendedores]
+
+
+def imprimir_reporte(totales):
     print("=" * ANCHO_SEPARADOR)
     print("    COMISIONES DEL MES - LA COMERCIAL")
     print("=" * ANCHO_SEPARADOR)
-    # recorre la lista
-    for vendedor in VENDEDORES:
-        # si vendio mas de 30000
-        if vendedor[1] > UMBRAL_COMISION_ALTA:
-            # calcula la comision del 8%
-            comision = vendedor[1] * TASA_COMISION_ALTA
-            comision = round(comision, 2)
-            # el bono es de 300
-            if vendedor[1] > UMBRAL_BONO:
-                bono = MONTO_BONO
-            else:
-                bono = 0
-            total_vendedor = round(comision + bono, 2)
-            total_pagado = total_pagado + total_vendedor
-            print(vendedor[0] + ": Q " + str(total_vendedor))
-        else:
-            # calcula la comision del 5%
-            comision = vendedor[1] * TASA_COMISION_BAJA
-            comision = round(comision, 2)
-            bono = 0
-            total_vendedor = round(comision + bono, 2)
-            total_pagado = total_pagado + total_vendedor
-            print(vendedor[0] + ": Q " + str(total_vendedor))
+    total_pagado = 0
+    for nombre, total in totales:
+        total_pagado = total_pagado + total
+        print(nombre + ": Q " + str(total))
     # ta = tp * 1.12
     # print("con iva", ta)
     print("-" * ANCHO_SEPARADOR)
     print("Total a pagar: Q " + str(round(total_pagado, 2)))
 
-calcular_comisiones()
+
+imprimir_reporte(calcular_comisiones(VENDEDORES))
